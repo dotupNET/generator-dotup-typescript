d94ad910c9884b04ea6971f03a65b7d01bccf74a
Peter Ullrich <p.ullrich@dotup.de>
2019-03-20 13:09:06 +0100

    test

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,28 +1,28 @@
-// tslint:disable: no-implicit-dependencies
-import * as path from 'path';
-import * as assert from 'yeoman-assert';
-import * as helpers from 'yeoman-test';
-import { ProjectQuestions, ProjectType } from '../src/app/ProjectGenerator';
-const ProjectGenerator = require('../src/app/index');
+// tslint:disable
+// import * as path from 'path';
+// import * as assert from 'yeoman-assert';
+// import * as helpers from 'yeoman-test';
+// import { ProjectQuestions, ProjectType } from '../src/app/ProjectGenerator';
+// const ProjectGenerator = require('../src/app/index');
 
-describe('generator-dotup-typescript:app', () => {
+// describe('generator-dotup-typescript:app', () => {
 
-  before(() => {
-    const answers = {
-      [ProjectQuestions.useGit]: false,
-      [ProjectQuestions.userEmail]: 'email',
-      [ProjectQuestions.userName]: 'name',
-      [ProjectQuestions.projectType]: ProjectType.ts_app_node,
-      [ProjectQuestions.createFolder]: false
-    };
-    return helpers
-      .run(ProjectGenerator)
-      .withPrompts(answers);
+//   before(() => {
+//     const answers = {
+//       [ProjectQuestions.useGit]: false,
+//       [ProjectQuestions.userEmail]: 'email',
+//       [ProjectQuestions.userName]: 'name',
+//       [ProjectQuestions.projectType]: ProjectType.ts_app_node,
+//       [ProjectQuestions.createFolder]: false
+//     };
+//     return helpers
+//       .run(ProjectGenerator)
+//       .withPrompts(answers);
 
-  });
+//   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
-  });
+//   it('creates files', () => {
+//     assert.file(['dummyfile.txt']);
+//   });
 
-});
+// });
