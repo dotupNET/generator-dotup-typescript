b98b906d37db13e84ba37d05476be5ecc4765bb7
Peter Ullrich <p.ullrich@dotup.de>
2019-03-21 16:09:28 +0100

    gulp

--- src/ts/templates/gulpfile.config.js
+++ src/ts/templates/gulpfile.config.js
@@ -1,39 +1,40 @@
 'use strict';
-const
-  fs = require('fs'),
-  path = require('path'),
-  config = require('./tools/gulp/gulp.json')
-  ;
 
 class GulpConfig {
 
   constructor() {
-    // Gulp files
-    this.gulpFiles = [];
 
     // source
-    this.sourcePath = '<%= sourcePath %>';
+    this.sourcePath = 'src';
     this.tsSourceFiles = this.sourcePath + '/**/*.ts';
 
     // test
-    this.testPath = '<%= testPath %>';
+    this.testPath = 'test';
     this.testFiles = `${this.testPath}/**/*.ts`;
+
     // target
-    this.targetPath = '<%= targetPath %>';
+    this.targetPath = 'generators';
+
     // docs
-    this.docsPath = '<%= docsPath %>';
+    this.docsPath = 'docs';
     this.docsFiles = this.docsPath + '/**/*';
 
-    this.loadAllFiles();
-  }
-
-  loadAllFiles() {
-    const gulps = fs.readdirSync('./tools/gulp').filter(file => path.extname(file) === '.js');
-    gulps.forEach(file => {
-      if (config[path.basename(file, '.js')] === true) {
-        this.gulpFiles.push(require('./tools/gulp/' + file));
+    // Static files
+    this.statics = [
+      {
+        sourcePath: `${this.sourcePath}/assets/**`,
+        targetPath: `${this.targetPath}/assets`
+      },
+      {
+        sourcePath: `${this.sourcePath}/**/templates/**`,
+        targetPath: `${this.targetPath}`
+      },
+      {
+        // Generator specific
+        sourcePath: `tools/gulp/**`,
+        targetPath: `${this.targetPath}/shared/gulp`
       }
-    });
+    ];
   }
 
 }
--- src/ts/templates/gulpfile.js
+++ src/ts/templates/gulpfile.js
@@ -1,50 +1,14 @@
 /**
  * Welcome to your gulpfile!
- * The gulp tasks are split in several files in the gulp directory for better modularity.
+ * The gulp tasks are split in several files in the tools/gulp directory for better modularity.
  */
-const
-  gulp = require('gulp'),
-  del = require('del'),
-  build = require('./tools/gulp/build'),
-  Config = require('./gulpfile.config')
-  ;
+const gulp = require('gulp');
+const GulpLoader = require('./tools/gulp/gulpLoader');
 
-// Load config and all gulp files.
-var config = new Config();
-config.loadAllFiles();
+// Load all gulp files.
+const gulpLoader = new GulpLoader();
+gulpLoader.loadAllFiles();
 
-gulp.task('project-build',
-  gulp.series(
-    build.clean,
-    build.compile,
-    assetsCopy
-  )
-);
-
-function assetsClean() {
-  return del([`${config.targetPath}/assets`]);
-}
-gulp.task('assets-clean', assetsClean);
-
-function assetsCopy() {
-  return gulp
-    .src(`${config.sourcePath}/assets/**`, { dot: true })
-    .pipe(gulp.dest(`${config.targetPath}/assets/`))
-    ;
-}
-gulp.task('assets-copy', assetsCopy);
-
-gulp.task('assets-watch', function () {
-  return gulp.watch([`${config.sourcePath}/assets`],
-    gulp.series('assets-clean', 'assets-copy')
-  );
-});
-
-gulp.task('watch-static',
-  gulp.parallel(
-    'assets-watch'
-  ),
-  function (done) {
-    done();
-  }
-);
+gulp.task('project-build', gulp.series(...gulpLoader.getProcess(gulpLoader.processNames.build)));
+gulp.task('project-publish', gulp.series(...gulpLoader.getProcess(gulpLoader.processNames.publish)));
+// gulp.task('project-watch', gulp.parallel(gulpLoader.getProcess(gulpLoader.processNames.watch)));
