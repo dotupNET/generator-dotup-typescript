f5cf7f038c6d0ea26ceae86daaad5e1c52cc5853
Peter Ullrich <p.ullrich@dotup.de>
2019-03-25 16:13:57 +0100

    gulp

--- tools/gulp/npm.js
+++ tools/gulp/npm.js
@@ -1,8 +1,14 @@
 const
-  spawn = require('cross-spawn')
+  spawn = require('cross-spawn'),
+  GitWrapper = require('dotup-ts-git-wrapper').GitWrapper
   ;
 
 function publish(done) {
+  const git = new GitWrapper();
+  if (git.hasChanges()) {
+    throw new Error('Can not be published with local changes. Commit and push first.');
+  }
+
   spawn.sync('npm', ['publish'], { stdio: 'inherit' });
   done();
 }
